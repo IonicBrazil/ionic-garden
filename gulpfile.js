141c910600a19c4b1de4042ab01101582872a2a9
Felquis Gimenes <frgformenton@gmail.com>
2015-06-21 17:26:13 -0300

    Working on src folder as the project source, and building to www

--- gulpfile.js
+++ gulpfile.js
@@ -8,30 +8,43 @@ var rename = require('gulp-rename');
 var sh = require('shelljs');
 
 var paths = {
-  sass: ['./scss/{,*/}*.scss']
+  sass: ['./src/scss/{,*/}*.scss'],
+  destCSS: 'www/css/',
+  html: ['./src/{,*/}*.html'],
+  outBase: 'www',
+  sourceBase: 'src'
 };
 
 gulp.task('default', ['sass']);
 
+// Build CSS to `www`
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
+  gulp.src(paths.sass)
     .pipe(sass({
       errLogToConsole: true
     }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest(paths.destCSS))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest(paths.destCSS))
     .on('end', done);
 });
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.html, ['html']);
 });
 
-gulp.task('install', ['git-check'], function() {
+// Build HTML to `www`
+gulp.task('html', function (done) {
+  gulp.src(paths.html, { base: paths.sourceBase})
+    .pipe(gulp.dest(paths.outBase));
+})
+
+// Install bower dependencies
+gulp.task('install', function() {
   return bower.commands.install()
     .on('log', function(data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
