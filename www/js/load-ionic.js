32c989b90483c352bade5c52ad19bd5973c65ef3
Felquis Gimenes <frgformenton@gmail.com>
2015-06-21 23:06:02 -0300

    Add load-ionic.js
    Load different versions of Ionic from local copy, or CND.
    
    Run `ionic serve` and put `?ionic=local` in your URL to load local copy
    (bower installed), `?ionic=build` to load local build, `?ionic=nightly`
    to load CDN nightly, or `?ionic=anyVersionFromCDN` to load files from
    CDN

---
+++ www/js/load-ionic.js
@@ -0,0 +1,34 @@
+(function () {
+
+  var options = (function () {
+    // Holy shit
+    var optionsArray = location.href.split('?')[1].split('#')[0].split('=')
+    var result = {}
+
+    optionsArray.forEach(function (value, index) {
+      // 0 is a property name and 1 the value of 0
+      if (index % 2 === 1) {
+        return
+      }
+
+      result[value] = optionsArray[index + 1]
+    });
+
+    return result
+  }())
+
+  var toLoadSrc;
+
+  if (options.ionic === 'build') {
+    toLoadSrc = 'lib/ionic/local/ionic.bundle.js'
+  } else if (options.ionic === 'bower' || !options.ionic) {
+    toLoadSrc = 'lib/ionic/js/ionic.bundle.js'
+  } else {
+    // Use options.ionic as ionic version
+    toLoadSrc = '//code.ionicframework.com/' + options.ionic + '/js/ionic.bundle.min.js'
+  }
+
+  console.log('Ionic From')
+  console.log(toLoadSrc)
+  document.write('<script src="'+ toLoadSrc +'"></script>')
+}())
