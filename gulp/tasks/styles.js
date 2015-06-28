1e36c18aa4ca204e31081e58cbdba3e41814f0ef
Felquis Gimenes <frgformenton@gmail.com>
2015-06-28 16:50:43 -0300

    Change SASS task to styles
    Checkout `gulp-plumber` for more information

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,17 +2,19 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
 var config = require('../config').common;
 
-gulp.task('sass', function(done) {
+gulp.task('styles', function(done) {
   gulp.src(config.allSCSSFiles)
-    .pipe(sass({
-      errLogToConsole: true
-    }))
+    .pipe(plumber())
+    .pipe(sass({ errLogToConsole: true }))
+    .pipe(autoprefixer({browsers: ['last 2 version', '> 5%']}))
     .pipe(gulp.dest(config.destCSS))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
+
+    // Minifield version
+    .pipe(minifyCss())
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest(config.destCSS))
     .on('end', done);
